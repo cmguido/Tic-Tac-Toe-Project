9522acd40edc7f0c59f00b16ff911dc9f0de876e
cmguido <chris.m.guido@gmail.com>
2017-08-30 17:20:03 -0400

    Commit first version CSS and start of game logic worked on inital code for game logic and css. Have to work out more, figure out handle and listening events.

---
+++ assets/scripts/gamelogic.js
@@ -0,0 +1,21 @@
+let turn = 1
+
+const squares = ['','','','','','','','','']
+
+const fill = function (index, player) {
+  if (squares[index] !== '') {
+    console.log(F OFF)
+  } else {
+    squares[index] = 'x'
+  }
+
+}
+
+
+
+
+
+
+module.exports {
+
+}
